176a9fd590c6b2e8cd24186c32a9215ca3f65ba4
Justin Pham <113923596+justin-phxm@users.noreply.github.com>
2024-03-24 20:04:57 +0000

    Update playbook.py to include HTTP request handling

--- run playbook.py
+++ run playbook.py
@@ -1,8 +1,20 @@
 import ansible_runner
-r = ansible_runner.run(private_data_dir='/tmp/demo', playbook='test.yml')
-print("{}: {}".format(r.status, r.rc))
-# successful: 0
-for each_host_event in r.events:
-    print(each_host_event['event'])
-print("Final status:")
-print(r.stats)
+import requests
+
+result = ansible_runner.interface.run(
+    private_data_dir='./',
+    playbook = "./hello.yml",
+    inventory = "./hosts.yml",
+)
+
+print(result.stats)
+
+for i in range(3):
+    try:
+        response = requests.get('http://0.0.0.0')
+        if response.ok:
+            print(response.text)
+        else:
+            print(f'Server on failed to respond with status code: {response.status_code}')
+    except requests.ConnectionError:
+        print(f'Server could not be reached.')
